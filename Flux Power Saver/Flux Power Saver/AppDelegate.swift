72d3fe47266745ad311453a164f6fec58409bbd7
Serge-Olivier Amega <sergeolivieramega@gmail.com>
2016-09-17 11:36:46 -0400

    added connection to appdelegate

--- Flux Power Saver/Flux Power Saver/AppDelegate.swift
+++ Flux Power Saver/Flux Power Saver/AppDelegate.swift
@@ -12,6 +12,7 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var connection: Connection?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
