15ca5b216f00cfd00d4945f206b1394531586953
Serge-Olivier Amega <sergeolivieramega@gmail.com>
2016-09-18 02:06:38 -0400

    added swiftyjson, alamofire

---
+++ Flux Power Saver/Pods/Target Support Files/Pods-Flux Power Saver/Pods-Flux Power Saver-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Flux_Power_Saver : NSObject
+@end
+@implementation PodsDummy_Pods_Flux_Power_Saver
+@end
---
+++ Flux Power Saver/Pods/Target Support Files/Pods-Flux Power Saver/Pods-Flux Power Saver-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Flux_Power_SaverVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Flux_Power_SaverVersionString[];
+
